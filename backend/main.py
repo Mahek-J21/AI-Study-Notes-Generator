6268f51aebd8fe46a5668d6a271d71192eb0f801
Mahek J-21 <jmahek3553@gmail.com>
2026-08-04 19:38:28 +0530

    Day 3: Added PDF upload endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,12 @@
-
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+import shutil
+import os
 
 app = FastAPI()
+
+UPLOAD_FOLDER = "uploads"
+
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @app.get("/")
 def home():
@@ -13,4 +18,16 @@ def about():
         "project": "AI Study Notes Generator",
         "version": "1.0",
         "developer": "Mahek J"
+    }
+
+@app.post("/upload")
+def upload_pdf(file: UploadFile = File(...)):
+    file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    return {
+        "message": "File uploaded successfully!",
+        "filename": file.filename
     }
